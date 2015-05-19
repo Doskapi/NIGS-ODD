217f886293f2616cd59a1b489bb83f3f23b8bfbc
Nicolas <nwaraya@gmail.com>
2015-05-19 17:34:46 -0300

    Hago el submit con el n grama que separa por palabra(ya no por letras)

--- Perceptron/main.cpp
+++ Perceptron/main.cpp
@@ -1,29 +1,52 @@
 //Inclusiones necesarias
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <list>
 
 using namespace std;
 
+int contarPalabras(string frase){
+    int cantidad = 0;
+    stringstream ss(frase);
+    string s;
+    while (getline(ss, s, ' ')) {
+        cantidad ++;
+    }
+    return cantidad;
+}
+
 //Dado una frase cualquiera y el tamanio del n grama devuleve
 list <string>  construirNgrama(string frase, int tamanioNgrama){
-	list <string> listaNgramas;
-	string unNgrama;
-	frase = ("$" + frase + "$");
-	int largo = frase.size();
-	for (int pos=0;(pos < (largo - tamanioNgrama + 1)); pos++){
-        unNgrama = "";
-        for (int i=0; (i < tamanioNgrama); i++){
-            unNgrama += frase.at(pos + i);
+    list <string> listaNgramas;
+    int cantidadPalabras = contarPalabras(frase);
+    if (cantidadPalabras > tamanioNgrama) {
+        stringstream unStream(frase);
+        string palabra1, palabra2, unNgrama;
+        int contadorNgrama = 0;
+        while (getline(unStream, palabra1, ' ')) {
+            unNgrama = "";
+            palabra2 = palabra1;
+            stringstream streamSegundario(frase);
+            for(int i= 0; i< contadorNgrama; i++){
+                getline(streamSegundario, palabra2, ' ');
+            }
+            for(int i= 0; i< tamanioNgrama; i++){
+                getline(streamSegundario, palabra2, ' ');
+                if (i < tamanioNgrama) unNgrama += palabra2 + " ";
+                else unNgrama += palabra2;
+            }
+            if ((contadorNgrama + tamanioNgrama ) <= cantidadPalabras){
+                    listaNgramas.push_back(unNgrama);
+            }
+            contadorNgrama++;
         }
-        listaNgramas.push_back(unNgrama);
-	}
+    } else listaNgramas.push_front(frase);
 	return listaNgramas;
 }
 
-int main()
-{
-    string frase = "una frase loca cualquiera en el para mostrar fontela bigote";
+int main() {
+    string frase = "una frase loca para mostrar que este n grama si funciona correctamente saludos";
     list<string> listaNgramas = construirNgrama(frase, 3);
     list<string>::iterator iterador; //Esta es la forma de recorrer esta lista
     int i = 0;
